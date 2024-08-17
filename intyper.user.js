b4485c6a2c0de0ecb92b06bb6bedd0969a7610f9
umserai <132711130+iscandalouz@users.noreply.github.com>
2024-08-17 15:20:03 -0300

    intyper-mobile.user.js

--- intyper.user.js
+++ intyper.user.js
@@ -1,27 +1,25 @@
-
 // ==UserScript==
-// @grant  		 none
-// @name         INTYPER
-// @namespace    INTYPER
-// @version      1.0.2
-// @description  MOSTRA POSSIVEIS RESPOSTAS DE DICAS NO GARTIZ
-// @author       THEO
-// @match        https://gartic.com.br/*
-// @run-at       document-end
-// @require		 http://code.jquery.com/jquery-3.3.1.min.js
-// @require		 https://raw.githubusercontent.com/iscandalouz/INTYPER/master/salas/alimentos.js
-// @require		 https://raw.githubusercontent.com/iscandalouz/INTYPER/master/salas/animais.js
-// @require		 https://raw.githubusercontent.com/iscandalouz/INTYPER/master/salas/bandeiras.js
-// @require		 https://raw.githubusercontent.com/iscandalouz/INTYPER/master/salas/desenho_animado.js
-// @require		 https://raw.githubusercontent.com/iscandalouz/INTYPER/master/salas/got.js
-// @require		 https://raw.githubusercontent.com/iscandalouz/INTYPER/master/salas/objetos.js
-// @require		 https://raw.githubusercontent.com/iscandalouz/INTYPER/master/salas/verbos.js
-// @require		 https://raw.githubusercontent.com/neekm/gartips/master/config.js
-// @require		 https://raw.githubusercontent.com/neekm/gartips/master/functions.js
-// @downloadURL  https://raw.githubusercontent.com/neekm/gartips/master/gartips.user.js
-// @updateURL    https://raw.githubusercontent.com/neekm/gartips/master/gartips.user.js
+// @grant         none
+// @name          INTYPER
+// @namespace     INTYPER
+// @version       1.0.2
+// @description   MOSTRA POSSIVEIS RESPOSTAS DE DICAS NO GARTIC
+// @author        THEO
+// @match         https://gartic.com.br/*
+// @run-at        document-end
+// @require       http://code.jquery.com/jquery-3.3.1.min.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/salas/alimentos.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/salas/animais.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/salas/bandeiras.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/salas/desenho_animado.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/salas/got.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/salas/objetos.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/salas/verbos.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/config.js
+// @require       https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/functions.js
+// @downloadURL   https://raw.githubusercontent.com/iscandalouzINTYPER-MOBILE/master/gartips.user.js
+// @updateURL     https://raw.githubusercontent.com/iscandalouz/INTYPER-MOBILE/master/gartips.user.js
 // ==/UserScript==
-
 
 /** ================
 *   FRONT JQUERY
@@ -41,8 +39,7 @@ $(function($) {
     // Colocando o botão de notificação para Mostrar ou Esconder o hack
     $('.opcoes').click(function() { // class do GARTIC
         if ($(this).attr('class') == 'opcoes gartips_hide') {
-            $('#gartips_botoes').hide();
-            $('#gartips_field_respostas').html('');
+            console.clear(); // Limpa o console
             $(this).addClass('gartips_show').removeClass('gartips_hide');
         } else {
             showBox();
@@ -50,24 +47,43 @@ $(function($) {
         }
     });
 
-    // gartips box
-    $('#tela').append('<div id="gartips" style="height:auto;"></div>');
-    $('#gartips').append('<div id="gartips_botoes" style="clear: both; display: none; padding-top:10px; text-align:right;"></div>');
+    // Adiciona botões e campos ao console
+    console.log('Adicionando opções ao console...');
+    console.log('Selecionar lista:');
+    $.each(salas, function(i, sala) {
+        console.log(`- ${sala.nome.toUpperCase()} (${sala.arr.length})`);
+    });
+
+    // Adiciona campo de pesquisa e botões
+    console.log('Campo de pesquisa e botões adicionados ao console.');
+
+    $('#tela').append('<div id="console" style="height:auto;"></div>');
+    $('#console').append('<div id="console_botoes" style="clear: both; padding-top:10px; text-align:right;"></div>');
 
     // Select com opções de listas
-    $('#gartips_botoes').append('<select id="gartips_select" style="height:39px; width:295px; text-align:center; float:left;"></select>');
+    $('#console_botoes').append('<select id="console_select" style="height:39px; width:295px; text-align:center; float:left;"></select>');
     $.each(salas, function(i, sala) {
-        $('#gartips_select').append(`<option value="${sala.nome.toLowerCase()}">${sala.nome.toUpperCase()} (${sala.arr.length})</option>`);
+        $('#console_select').append(`<option value="${sala.nome.toLowerCase()}">${sala.nome.toUpperCase()} (${sala.arr.length})</option>`);
     });
 
     // Botões
-    $('#gartips_botoes').append('<input type="text" id="gartips_search" placeholder="palavra chave" style="text-align:center; margin-left:5px; width:350px; height:30px; float:left" />');
-    $('#gartips_botoes').append('<button id="gartips_get_respostas" style="margin-left:5px; width:290px; height:39px; text-align:center; float:left;">Possiveis Respostas</button>');
-    $('#gartips_get_respostas').click(function() { getRespostas() });
-    $('#gartips_botoes').append('<button id="gartips_limpa_respostas" style="margin-left:5px; width: 172px; height:39px; float:left;">Limpar Respostas</button>');
-    $('#gartips_limpa_respostas').click(function() { limparRespostas() });
+    $('#console_botoes').append('<input type="text" id="console_search" placeholder="palavra chave" style="text-align:center; margin-left:5px; width:350px; height:30px; float:left" />');
+    $('#console_botoes').append('<button id="console_get_respostas" style="margin-left:5px; width:290px; height:39px; text-align:center; float:left;">Possíveis Respostas</button>');
+    $('#console_get_respostas').click(function() { getRespostas() });
+    $('#console_botoes').append('<button id="console_limpa_respostas" style="margin-left:5px; width: 172px; height:39px; float:left;">Limpar Respostas</button>');
+    $('#console_limpa_respostas').click(function() { limparRespostas() });
 
-    // Resposta
-    $('#gartips_botoes').append('<div id="gartips_field_respostas" style="clear:both; color:#000; font-size:18px;padding:10px; text-align:left; "></div>');
-
+    // Adiciona a div de respostas ao console
+    $('#console_botoes').append('<div id="console_field_respostas" style="clear:both; color:#000; font-size:18px;padding:10px; text-align:left; "></div>');
 });
+
+function getRespostas() {
+    console.log('Obtendo respostas...');
+    // Implementar a lógica de obtenção das respostas e logar no console
+}
+
+function limparRespostas() {
+    console.log('Limpando respostas...');
+    // Implementar a lógica para limpar as respostas e logar no console
+}
+
